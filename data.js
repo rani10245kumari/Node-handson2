a3a87b0a120ba10dc40a600e2a241c56ad5ec4d8
rani10245kumari <ranik10245@gmail.com>
2023-11-26 21:48:13 +0530

    first

---
+++ data.js
@@ -0,0 +1,18 @@
+const data = [{
+
+    name: "deepu",
+    course: "upsc"
+}]
+const data1 = [{
+    name: "rani",
+    course: "bca"
+}]
+const data2 = [{
+    name: "pihu",
+    class: "mca"
+}]
+const data3 = [{
+    name: "momo",
+    class: "kg"
+}]
+module.exports = { data, data1, data2, data3 }
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express")
+const { data, data1, data2, data3 } = require("./data")
 const app = express()
+
 app.get('/', (req, res) => {
     res.send("Express is a web application framework for Node.js. It is designed to simplify the process of building web applications and APIs by providing a set of features and tools for handling common tasks, such as routing, middleware, and template engines. Express is known for its simplicity and flexibility, making it a popular choice for developers working with Node.js")
+})
+app.get('/value', (req, res) => {
+    res.send(data2)
 })
 app.listen(5600, () => {
     try {
